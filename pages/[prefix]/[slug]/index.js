223f0c51c90364ee0ae10eecd6af28a8d5fa18ec
dogdogw <147697609+Hendrix0707@users.noreply.github.com>
2026-05-14 15:36:43 +0800

    Update index.js

--- pages/[prefix]/[slug]/index.js
+++ pages/[prefix]/[slug]/index.js
@@ -3,6 +3,7 @@ import { siteConfig } from '@/lib/config'
 import { resolvePostProps } from '@/lib/db/SiteDataApi'
 import Slug from '..'
 import { checkSlugHasOneSlash } from '@/lib/utils/post'
+import { isExport } from '@/lib/utils/buildMode'
 import { getStaticPathsBase } from '@/lib/build/staticPaths'
 
 /**
